e650480d32f40a923a639be3204914a09cb4fee0
Neil <neilaylor@hotmail.com>
2018-02-25 14:21:16 -0600

    .

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -2,34 +2,6 @@ class SwipesController < ApplicationController
   def index
     begin
       RSpotify.authenticate("5fc8d642deb74674848fee0fd969256e", "b5e3187924da4848b8a0183f509fe46e")
-      @genres = Rails.application.config.categories
-
-      @genres = @genres.uniq
-
-      @genres.map!(&:downcase)
-
-      recommendations = RSpotify::Recommendations.generate(seed_genres: @genres)
-
-      @array_of_tracks = []
-
-      recommendations.tracks.each do |track|
-        h = Hash.new
-
-        h[:name] = track.name
-        h[:artist] = track.artists[0].name
-        h[:duration] = get_duration_hrs_and_mins(track.duration_ms).to_s
-        h[:url] = track.preview_url
-
-        if h[:url] != nil
-          @array_of_tracks.push(h)
-        end
-
-      end
-
-      Rails.application.config.array_of_tracks = @array_of_tracks
-
-      Rails.application.config.question_array = []
-
     rescue => e
       render 'traffic_error'
     end
